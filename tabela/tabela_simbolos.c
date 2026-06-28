85bd80bb8471b8edfa97d23b65004dab07243f88
leandrofelix20k <leandro.almeida3029@gmail.com>
2026-06-28 12:54:16 -0300

    Corrigido os problemas 01 e 04, criando agora de forma correta os tipos racionais

--- tabela/tabela_simbolos.c
+++ tabela/tabela_simbolos.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 static Symbol * table[MAX_SYMBOLS];
+static UserType * user_types = NULL;
 
 static ScopeEntry scope_stack[MAX_SCOPES];
 static int scope_sp = 0;
@@ -16,6 +17,7 @@ static unsigned int hash(const char * key) {
 
 void symtable_init() {
     memset(table, 0, sizeof(table));
+    user_types = NULL;
     scope_sp = 0;
 }
 
@@ -29,6 +31,19 @@ void symtable_free() {
         }
         table[i] = NULL;
     }
+
+    while (user_types) {
+        UserType * next_type = user_types->next;
+        TypeField * field = user_types->fields;
+        while (field) {
+            TypeField * next_field = field->next;
+            free(field);
+            field = next_field;
+        }
+        free(user_types);
+        user_types = next_type;
+    }
+
     scope_sp = 0;
 }
 
@@ -38,8 +53,19 @@ void scope_push(const char * name, VarType ret_type) {
         exit(1);
     }
     strncpy(scope_stack[scope_sp].name, name, MAX_KEY_LEN - 1);
+    scope_stack[scope_sp].name[MAX_KEY_LEN - 1] = '\0';
     scope_stack[scope_sp].return_type = ret_type;
+    strncpy(scope_stack[scope_sp].return_declared_type, type_name(ret_type), MAX_KEY_LEN - 1);
+    scope_stack[scope_sp].return_declared_type[MAX_KEY_LEN - 1] = '\0';
     scope_sp++;
+}
+
+void scope_set_return_type(VarType ret_type, const char * declared_type) {
+    if (scope_sp <= 0) return;
+    scope_stack[scope_sp - 1].return_type = ret_type;
+    strncpy(scope_stack[scope_sp - 1].return_declared_type,
+            declared_type ? declared_type : type_name(ret_type), MAX_KEY_LEN - 1);
+    scope_stack[scope_sp - 1].return_declared_type[MAX_KEY_LEN - 1] = '\0';
 }
 
 void scope_pop() {
@@ -84,12 +110,45 @@ int sym_insert(const char * name, VarType type, int is_function, VarType ret_typ
     strncpy(sym->key, key, MAX_KEY_LEN - 1);
     strncpy(sym->name, name, MAX_KEY_LEN - 1);
     strncpy(sym->scope, current_scope, MAX_KEY_LEN - 1);
+    sym->key[MAX_KEY_LEN - 1] = '\0';
+    sym->name[MAX_KEY_LEN - 1] = '\0';
+    sym->scope[MAX_KEY_LEN - 1] = '\0';
     sym->type = type;
+    strncpy(sym->declared_type, type_name(type), MAX_KEY_LEN - 1);
+    sym->declared_type[MAX_KEY_LEN - 1] = '\0';
     sym->is_function = is_function;
     sym->return_type = ret_type;
+    strncpy(sym->return_declared_type, type_name(ret_type), MAX_KEY_LEN - 1);
+    sym->return_declared_type[MAX_KEY_LEN - 1] = '\0';
     sym->next = table[h];
     table[h] = sym;
     return 1;
+}
+
+void sym_set_declared_type(const char * name, const char * declared_type) {
+    Symbol * sym = sym_lookup_local(name);
+    if (!sym || !declared_type) return;
+    strncpy(sym->declared_type, declared_type, MAX_KEY_LEN - 1);
+    sym->declared_type[MAX_KEY_LEN - 1] = '\0';
+}
+
+int sym_set_function_return(const char * name, VarType return_type, const char * declared_type) {
+    char key[MAX_KEY_LEN];
+    make_key("global", name, key);
+    unsigned int h = hash(key);
+    Symbol * sym = table[h];
+
+    while (sym) {
+        if (strcmp(sym->key, key) == 0 && sym->is_function) {
+            sym->return_type = return_type;
+            strncpy(sym->return_declared_type,
+                    declared_type ? declared_type : type_name(return_type), MAX_KEY_LEN - 1);
+            sym->return_declared_type[MAX_KEY_LEN - 1] = '\0';
+            return 1;
+        }
+        sym = sym->next;
+    }
+    return 0;
 }
 
 Symbol * sym_lookup(const char * name) {
@@ -119,13 +178,69 @@ Symbol * sym_lookup_local(const char * name) {
     return NULL;
 }
 
+int user_type_insert(const char * name) {
+    UserType * type;
+
+    if (user_type_lookup(name)) return 0;
+
+    type = malloc(sizeof(UserType));
+    if (!type) { fprintf(stderr, "Sem memória\n"); exit(1); }
+    strncpy(type->name, name, MAX_KEY_LEN - 1);
+    type->name[MAX_KEY_LEN - 1] = '\0';
+    type->fields = NULL;
+    type->next = user_types;
+    user_types = type;
+    return 1;
+}
+
+UserType * user_type_lookup(const char * name) {
+    UserType * type = user_types;
+    while (type) {
+        if (strcmp(type->name, name) == 0) return type;
+        type = type->next;
+    }
+    return NULL;
+}
+
+int user_type_add_field(const char * type_name_value, const char * field_name,
+                        VarType field_type, const char * declared_type) {
+    UserType * type = user_type_lookup(type_name_value);
+    TypeField * field;
+
+    if (!type || user_type_field_lookup(type_name_value, field_name)) return 0;
+
+    field = malloc(sizeof(TypeField));
+    if (!field) { fprintf(stderr, "Sem memória\n"); exit(1); }
+    strncpy(field->name, field_name, MAX_KEY_LEN - 1);
+    field->name[MAX_KEY_LEN - 1] = '\0';
+    strncpy(field->declared_type, declared_type, MAX_KEY_LEN - 1);
+    field->declared_type[MAX_KEY_LEN - 1] = '\0';
+    field->type = field_type;
+    field->next = type->fields;
+    type->fields = field;
+    return 1;
+}
+
+TypeField * user_type_field_lookup(const char * type_name_value, const char * field_name) {
+    UserType * type = user_type_lookup(type_name_value);
+    TypeField * field;
+
+    if (!type) return NULL;
+    field = type->fields;
+    while (field) {
+        if (strcmp(field->name, field_name) == 0) return field;
+        field = field->next;
+    }
+    return NULL;
+}
+
 const char * type_name(VarType t) {
     switch (t) {
         case TYPE_BOOL:     return "bool";
         case TYPE_INT:      return "int";
         case TYPE_FLOAT:    return "float";
         case TYPE_STRING:   return "char*";
-        case TYPE_RATIONAL: return "rational_t";
+        case TYPE_USER_DEFINED: return "registro";
         case TYPE_VOID:     return "void";
         default:            return "desconhecido";
     }
@@ -136,10 +251,23 @@ VarType type_from_string(const char * s) {
     if (strcmp(s, "int")   == 0) return TYPE_INT;
     if (strcmp(s, "float") == 0) return TYPE_FLOAT;
     if (strcmp(s, "texto") == 0 || strcmp(s, "char*") == 0) return TYPE_STRING;
-    if (strcmp(s, "rational_t") == 0) return TYPE_RATIONAL;
     if (strcmp(s, "vazio") == 0 || strcmp(s, "void") == 0) return TYPE_VOID;
+    if (user_type_lookup(s)) return TYPE_USER_DEFINED;
     
     return TYPE_UNKNOWN;
+}
+
+int declared_types_compatible(const char * type1, const char * type2) {
+    VarType t1;
+    VarType t2;
+
+    if (!type1 || !type2) return 0;
+    if (strcmp(type1, type2) == 0) return 1;
+
+    t1 = type_from_string(type1);
+    t2 = type_from_string(type2);
+    if (t1 == TYPE_USER_DEFINED || t2 == TYPE_USER_DEFINED) return 0;
+    return types_compatible(t1, t2);
 }
 
 int types_compatible(VarType t1, VarType t2) {
--- tabela/tabela_simbolos.h
+++ tabela/tabela_simbolos.h
@@ -10,42 +10,67 @@ typedef enum {
     TYPE_INT,
     TYPE_FLOAT,
     TYPE_STRING,
-    TYPE_RATIONAL,
+    TYPE_USER_DEFINED,
     TYPE_VOID,
     TYPE_UNKNOWN
 } VarType;
 
+typedef struct TypeField {
+    char name[MAX_KEY_LEN];
+    char declared_type[MAX_KEY_LEN];
+    VarType type;
+    struct TypeField * next;
+} TypeField;
+
+typedef struct UserType {
+    char name[MAX_KEY_LEN];
+    TypeField * fields;
+    struct UserType * next;
+} UserType;
 
 typedef struct Symbol {
     char key[MAX_KEY_LEN];   
     char name[MAX_KEY_LEN];
     char scope[MAX_KEY_LEN];
     VarType type;
+    char declared_type[MAX_KEY_LEN];
     int is_function;
-    VarType return_type;     
+    VarType return_type;
+    char return_declared_type[MAX_KEY_LEN];
     struct Symbol * next;    
 } Symbol;
 
 
 typedef struct {
     char name[MAX_KEY_LEN]; 
-    VarType return_type;     
+    VarType return_type;
+    char return_declared_type[MAX_KEY_LEN];
 } ScopeEntry;
 
 void symtable_init();
 void symtable_free();
 
 void scope_push(const char * name, VarType ret_type);
+void scope_set_return_type(VarType ret_type, const char * declared_type);
 void scope_pop();
 ScopeEntry * scope_top();
 int scope_depth();
 
 int sym_insert(const char * name, VarType type, int is_function, VarType ret_type);
+void sym_set_declared_type(const char * name, const char * declared_type);
+int sym_set_function_return(const char * name, VarType return_type, const char * declared_type);
 Symbol * sym_lookup(const char * name);   
 Symbol * sym_lookup_local(const char * name); 
+
+int user_type_insert(const char * name);
+UserType * user_type_lookup(const char * name);
+int user_type_add_field(const char * type_name, const char * field_name,
+                        VarType field_type, const char * declared_type);
+TypeField * user_type_field_lookup(const char * type_name, const char * field_name);
 
 const char * type_name(VarType t);
 VarType type_from_string(const char * s);
 int types_compatible(VarType t1, VarType t2);
+int declared_types_compatible(const char * type1, const char * type2);
 
 #endif
